592794e55e914c2f078c1a8fb24650b1a65081f5
Vandlik Viktor <h150209@stud.u-szeged.hu>
2023-04-25 12:36:34 +0200

    megjegyzesek elkeszitese

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -3,8 +3,10 @@
     <p>Copyright 2023</p>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="megjegyzes.js"></script>
 
 <?php
 // Close the Oracle connection
