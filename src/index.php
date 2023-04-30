eee61782f7ec9808b615e94bd369f507e2344ddd
Vandlik Viktor <h150209@stud.u-szeged.hu>
2023-04-30 15:52:51 +0200

    főoldal

--- src/index.php
+++ src/index.php
@@ -22,36 +22,39 @@
     </header>
 
     <main>
-        <div class="container">
-            <h1>Tudásadatbázis</h1>
-            <div class="btn-group" role="group" aria-label="Basic outlined example">
-                <button type="button" class="btn btn-outline-primary">Cikkek</button>
-                <button type="button" class="btn btn-outline-primary">Felhasználók</button>
-            </div>
-        </div>
+        
         <div class="container">  
         <h1>Üdvözöllek a Tudásadatbázisban!</h1>
             <div>Ezt a webes alkalmazást azért hoztuk létre, hogy bárki ingyenesen elérhessen rövid ismeretterjesztő cikkeket.
             Ha egy témakörön belül keresel valamit, kattints a cikkekre majd a listázásra.
-            Itt témakörökre bontva megtekintheted a már publikált cikkeket.</div>
+            Itt témakörökre bontva megtekintheted a már publikált cikkeket.</div></br>
             <div>Amennyiben komolyabban érdekel kicsiny kis oldalunk, a profil -> regisztráció menüpontban regisztrálhatsz, ezáltal új funkciókat érhetsz el.
             Regisztráció után a profil -> bejelentkezés fülnél be tudsz jelentkezni.
-            Ezután a cikkek alá megjegyzéseket is írhatsz, melyek a felhasználó neveddel megjelennek a cikk alatt.</div>
-            <div>Amennyiben cikkeket szeretnél írni, írj egy emailt az admin@admin.hu email címre egy rövid bemutatkozó szöveggel és, hogy milyen témakörökben szeretbél tartalmat írni.
-            Kérésedet 24 órán belül elbíráljuk, majd visszajelzést küldünk. Ha elfogadtuk, következő bejelentkezésed alkalmával már szerző jogokkal is rendelkezel.
+            Ezután a cikkek alá megjegyzéseket is írhatsz, melyek a felhasználó neveddel megjelennek a cikk alatt.</div></br>
+            <div>Amennyiben cikkeket szeretnél írni, írj egy emailt az admin@admin.hu email címre egy rövid bemutatkozó szöveggel és, hogy milyen témakörökben szeretnél tartalmat írni.
+            Kapni fogsz egy tájékoztatót, a szerzőkre, lektorokra vonatkozó kötelezettségekről, szabályokról majd kérésedet 24 órán belül elbíráljuk, majd visszajelzést küldünk. Ha elfogadtuk, következő bejelentkezésed alkalmával már szerző jogokkal is rendelkezel.
             Szerzőként írhatsz cikkeket. A cikknek adj egy címet, sorold be egy már létező kategóriába, vagy hozz létre új kategóriát.
             Kulcsszavakat is megadhatsz, így megkönnyítve, hogy mások megtalálják a cikket.
             Beküldés után a cikkedet egy lektornak jóvá kell hagyni. A cikkeidet az általam írt cikkek menüpont alatt találhatod meg.
             Itt még publikálás előtt módosíthatsz rajta vagy akár törölheted is.
-            Miután egy lektor elfogadta a cikkedet, az megjelenik mindenki számára. Később is módosíthatod, vagy törölheted.</div>
+            Miután egy lektor elfogadta a cikkedet, az megjelenik mindenki számára. Később is módosíthatod, vagy törölheted.</div></br>
             <div>Lektor rangot is az admin@admin.hu email címen tudsz igényelni. Ennek feltételei a következők:
-                -Legalább 1 hónapja regisztrált felhasználónak kell lenned.
-                -Legalább 5 publikált cikkel kell rendelkezned.
-                -Vállalnod kell, hogy havonta legalább 5 cikket ellenőrzöl.
+            <ul>
+                <li>Legalább 1 hónapja regisztrált felhasználónak kell lenned.</li>
+                <li>Legalább 5 publikált cikkel kell rendelkezned.</li>
+                <li>Vállalnod kell, hogy havonta legalább 5 cikket ellenőrzöl.</li>
+            </ul>    
             Miután megkaptad a lektor rangot, elérheted az ellenőrzés menüpontot. Itt találod a publikálatlan cikkeket, amelyeket publikálni, módosítani vagy törölni tudsz.
-            Lektorként a publikált cikkeket is módosíthatod vagy törölheted.</div>
-            <div> A legmagasabb rang, az admin. Egy admin megtekintheti a felhasználókat, rangokat oszthat ki, vagy vehet el valamint felhasználókat törölhet.
+            Lektorként a publikált cikkeket is módosíthatod vagy törölheted.</div></br>
+            <div> A legmagasabb rang, az admin. Egy admin megtekintheti a felhasználókat, rangokat oszthat ki, vagy vehet el, valamint felhasználókat törölhet.
             Jelenleg nincs nyitott pozíció admin rangra. Amennyiben lesz, a főoldalon hirdetünk meg felvételt ezért érdemes rendszeresen figyelni.</div>
+        </div></br>
+        <div class="container">
+            <h2>Elérhetőségeink</h2>
+            <ul>
+                <li>Email: admin@admin.hu</li>
+                <li>Tel: 06201234567 (hétköznap 8:00 és 15:00 között)</li>
+            </ul>
         </div>
     </main>
 
