3632ecfa1eb1123f5cab44435c1516d2a8f10089
Attila213 <attilamolnr@outlook.hu>
2023-04-30 13:22:40 +0200

    kulcsszo insert

--- src/cikk_beszuras.php
+++ src/cikk_beszuras.php
@@ -34,73 +34,38 @@ if(isset($_POST['content']) && isset($_POST['cim'])){
     
         oci_execute($v);
     
+        //--------------------------------------------
+        $kulcsok = explode(',',$_POST['keresoszavak']);
+
+        $array = oci_parse($conn, "SELECT MAX(id) from CIKK");
+        oci_execute($array);
+        $row = oci_fetch_array($array);
+        for($i = 0;$i < count($kulcsok);$i++){
+            $v = oci_parse($conn,"INSERT INTO KULCSSZO(CIKK_ID,KULCSSZO) VALUES(:cid,:kat)");
+    
+    
         
-    }
+            oci_bind_by_name($v,":cid",$row[0]);
+            oci_bind_by_name($v,":kat",$kulcsok[$i]);
+            echo "<br>";
+            oci_execute($v);
+        }
+    } 
     header("Location: cikk_iras.php");
 }
 ?>
 
 
-<?php
-/*
-include 'resources/functions/config.php'; 
-//ha szerző akkor
-if(isset($_POST['content']) && isset($_POST['cim']) && isset($_POST['keresoszavak'])){    
-    $v = oci_parse($conn,"INSERT INTO CIKK(SZERZO_ID,LEKTOR_ID,CIM,TARTALOM,ALLAPOT) VALUES(:fid,NULL,:cim,:content,'in process')");
-=======
-// TODO: ha szerző akkor
-if(isset($_POST['content']) && isset($_POST['cim'])){
-    // Frissítés
-    if(isset($_POST['id'])){
-        $v = oci_parse($conn,"UPDATE CIKK SET CIM = '"+$_POST['cim']+"', TARTALOM = '"+$_POST['content']+"') WHERE id = "+$_POST['id']);
-        oci_execute($v);
->>>>>>> a2bb04c86cf4b48c689ef1160f06b2daae0fe2d1
 
-        $v = oci_parse($conn,"UPDATE KATEGORIA SET KATEGORIA = '".$_POST['kat']."' WHERE CIKK_ID = ".$_POST['id']);
-        oci_execute($v);
-    // létrehozás
-    } else {
-        $v = oci_parse($conn,"INSERT INTO CIKK(SZERZO_ID,LEKTOR_ID,CIM,TARTALOM,ALLAPOT) VALUES(:fid,NULL,:cim,:content,'in process')");
 
-        oci_bind_by_name($v,":fid",$_SESSION['felhasznalo']['id']);
-        oci_bind_by_name($v,":cim",$_POST['cim']);
-        oci_bind_by_name($v,":content",$_POST['content']);
-    
-<<<<<<< HEAD
-    oci_execute($array);
-
-    $row = oci_fetch_array($array);
-
-    oci_bind_by_name($v,":cid",$row[0]);
-    oci_bind_by_name($v,":kat",$_POST['kat']);
-
-    oci_execute($v);
-
-//--------------------------------------------------------------
-
-    $kulcsok = explode(',',$_POST['keresoszavak']);
-
-    $array = oci_parse($conn, "SELECT MAX(id) from CIKK");
-    oci_execute($array);
-    $row = oci_fetch_array($array);
-    for($i = 0;$i < count($kulcsok);$i++){
-        $v = oci_parse($conn,"INSERT INTO KULCSSZO(CIKK_ID,KULCSSZO) VALUES(:cid,:kat)");
 
 
     
-        oci_bind_by_name($v,":cid",$row[0]);
-        oci_bind_by_name($v,":kat",$kulcsok[$i]);
-        echo "<br>";
-        oci_execute($v);
-
-    }
 
 
 
 
-    header("Location: cikk_iras.php");
-}
 
 
-*/
-?>
+
+
